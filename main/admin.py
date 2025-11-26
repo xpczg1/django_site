0250e832876640ff620b8b5d436fc04a9862665a
bloodyduxxx-dotcom <bloodyduxxx@gmail.com>
2025-11-26 21:13:18 +0900

    Add files via upload

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Roles, Users
 
-# Register your models here.
+@admin.register(Roles)
+class RolesAdmin(admin.ModelAdmin):
+    list_display = ('ID_Role', 'Name_Role')
+    search_fields = ('Name_Role',)
+
+@admin.register(Users)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('Login', 'Name', 'Surname', 'ID_Role', 'is_active')
+    list_filter = ('ID_Role', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('Login', 'password')}),
+        ('Персональная информация', {'fields': ('Name', 'Surname', 'Patronymic', 'ID_Role')}),
+        ('Права доступа', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Важные даты', {'fields': ('last_login',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('Login', 'Name', 'Surname', 'Patronymic', 'ID_Role', 'password1', 'password2', 'is_active', 'is_staff'),
+        }),
+    )
+    search_fields = ('Login', 'Name', 'Surname')
+    ordering = ('Login',)
--- main/urls.py
+++ main/urls.py
@@ -1,26 +1,23 @@
-"""
-URL configuration for myproject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-
-from django.urls import path
-from . import views
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from users import views as user_views
+from main import views as main_views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('map/', views.map_view, name='map'),
+    # Admin site
+    path('admin/', admin.site.urls),
+
+    # Home page
+    path('', main_views.home, name='home'),
+
+    # Authentication URLs
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+
+    # App URLs
+    path('profile/', user_views.profile, name='profile'),
+    path('map/', user_views.map_view, name='map'),
+    path('objects/', main_views.object_list, name='object-list'),
 ]
